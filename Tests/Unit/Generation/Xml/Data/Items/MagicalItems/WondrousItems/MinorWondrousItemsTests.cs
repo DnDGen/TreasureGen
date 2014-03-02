ae92e6db3f6634685636cee7cc952e6a16ce9cf3
Karl Speer <cidthecoatrack@gmail.com>
2014-03-02 14:35:16 -0500

    awrogsbu
    more stuff

---
+++ Tests/Unit/Generation/Xml/Data/Items/MagicalItems/WondrousItems/MinorWondrousItemsTests.cs
@@ -0,0 +1,27 @@
+﻿using EquipmentGen.Tests.Unit.Generation.Xml.Data.Attributes;
+using NUnit.Framework;
+
+namespace EquipmentGen.Tests.Unit.Generation.Xml.Data.Items.MagicalItems.WondrousItems
+{
+    [TestFixture, PercentileTable("MinorWondrousItems")]
+    public class MinorWondrousItemsTests : PercentileTests
+    {
+        [Test]
+        public void QuaalsFeatherTokenAnchorPercentile()
+        {
+            AssertContent("Quaal's feather token, anchor", 1);
+        }
+
+        [Test]
+        public void UniversalSolventPercentile()
+        {
+            AssertContent("Universal solvent", 2);
+        }
+
+        [Test]
+        public void ElixerOfLovePercentile()
+        {
+            AssertContent("Elixer of love", 3);
+        }
+    }
+}
