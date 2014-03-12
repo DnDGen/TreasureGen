3e222d3e07830d4d7761c77ddd8a9790ca34a4a0
Karl Speer <kspeer@qualtrax.com>
2014-03-12 15:40:24 -0400

    Major wondrous items tests
    * Added the rests of the tests for major wondrous items.

--- Tests/Unit/Generation/Xml/Data/Items/MagicalItems/WondrousItems/MajorWondrousItemsTests.cs
+++ Tests/Unit/Generation/Xml/Data/Items/MagicalItems/WondrousItems/MajorWondrousItemsTests.cs
@@ -11,5 +11,599 @@ namespace EquipmentGen.Tests.Unit.Generation.Xml.Data.Items.MagicalItems.Wondrou
         {
             AssertContent("Dimensional shackles", 1);
         }
+
+        [Test]
+        public void ObsidianSteedFigurineOfWondrousPowerPercentile()
+        {
+            AssertContent("Obsidian steed figurine of wondrous power", 2);
+        }
+
+        [Test]
+        public void DrumsOfPanicPercentile()
+        {
+            AssertContent("Drums of panic", 3);
+        }
+
+        [Test]
+        public void OrangeIounStonePercentile()
+        {
+            AssertContent("Orange ioun stone", 4);
+        }
+
+        [Test]
+        public void PaleGreenPrismIounStonePercentile()
+        {
+            AssertContent("Pale green prism ioun stone", 5);
+        }
+
+        [Test]
+        public void LanternOfRevealingPercentile()
+        {
+            AssertContent("Lantern of revealing", 6);
+        }
+
+        [Test]
+        public void RobeOfBlendingPercentile()
+        {
+            AssertContent("Robe of blending", 7);
+        }
+
+        [Test]
+        public void AmuletOfNaturalArmorPlus4Percentile()
+        {
+            AssertContent("Amulet of natural armor +4", 8);
+        }
+
+        [Test]
+        public void AmuletOfProofAgaisntDetectionAndLocationPercentile()
+        {
+            AssertContent("Amulet of proof against detection and location", 9);
+        }
+
+        [Test]
+        public void CarpetOfFlying5x10Percentile()
+        {
+            AssertContent("5 ft. by 10 ft. carpet of flying", 10);
+        }
+
+        [Test]
+        public void IronGolemManualPercentile()
+        {
+            AssertContent("Iron golem manual", 11);
+        }
+
+        [Test]
+        public void AmuletOfHealthPlus6Percentile()
+        {
+            AssertContent("Amulet of health +6", 12);
+        }
+
+        [Test]
+        public void BeltOfGiantStrengthPlus6Percentile()
+        {
+            AssertContent("Belt of giant strength +6", 13);
+        }
+
+        [Test]
+        public void BracersOfArmorPlus6Percentile()
+        {
+            AssertContent("Bracers of armor +6", 14);
+        }
+
+        [Test]
+        public void CloakOfCharismaPlus6Percentile()
+        {
+            AssertContent("Cloak of charisma +6", 15);
+        }
+
+        [Test]
+        public void GlovesOfDexterityPlus6Percentile()
+        {
+            AssertContent("Gloves of dexterity +6", 16);
+        }
+
+        [Test]
+        public void HeadbandOfIntellectPlus6Percentile()
+        {
+            AssertContent("Headband of intellect +6", 17);
+        }
+
+        [Test]
+        public void VibrantPurplePrismIounStonePercentile()
+        {
+            AssertContent("Vibrant purple prism ioun stone", 18);
+        }
+
+        [Test]
+        public void SixthLevelSpellPearlOfPowerPercentile()
+        {
+            AssertContent("6th-level spell pearl of power", 19);
+        }
+
+        [Test]
+        public void PeriaptOfWisdomPlus6Percentile()
+        {
+            AssertContent("Periapt of wisdom +6", 20);
+        }
+
+        [Test]
+        public void ScarabOfProtectionPercentile()
+        {
+            AssertContent("Scarab of protection", 21);
+        }
+
+        [Test]
+        public void LavenderAndGreenEllipsoidIounStonePercentile()
+        {
+            AssertContent("Lavender and green ellipsoid ioun stone", 22);
+        }
+
+        [Test]
+        public void RingGatesPercentile()
+        {
+            AssertContent("Ring gates", 23);
+        }
+
+        [Test]
+        public void CrystalBallPercentile()
+        {
+            AssertContent("Crystal ball", 24);
+        }
+
+        [Test]
+        public void GreaterStoneGolemManualPercentile()
+        {
+            AssertContent("Greater stone golem manual", 25);
+        }
+
+        [Test]
+        public void OrbOfStormsPercentile()
+        {
+            AssertContent("Orb of storms", 26);
+        }
+
+        [Test]
+        public void BootsOfTeleportationPercentile()
+        {
+            AssertContent("Boots of teleportation", 27);
+        }
+
+        [Test]
+        public void BracersOfArmorPlus7Percentile()
+        {
+            AssertContent("Bracers of armor +7", 28);
+        }
+
+        [Test]
+        public void SeventhLevelSpellPearlOfPowerPercentile()
+        {
+            AssertContent("7th-level spell pearl of power", 29);
+        }
+
+        [Test]
+        public void AmuletOfNaturalArmorPlus5Percentile()
+        {
+            AssertContent("Amulet of natural armor +5", 30);
+        }
+
+        [Test]
+        public void MajorCloakOfDisplacementPercentile()
+        {
+            AssertContent("Major cloak of displacement", 31);
+        }
+
+        [Test]
+        public void CrystallBallWithSeeInvisibilityPercentile()
+        {
+            AssertContent("Crystal ball with see invisibility", 32);
+        }
+
+        [Test]
+        public void HornOfValhallaPercentile()
+        {
+            AssertContent("Horn of Valhalla", 33);
+        }
+
+        [Test]
+        public void CrystalBallWithDetectThoughtsPercentile()
+        {
+            AssertContent("Crystal ball with detect thoughts", 34);
+        }
+
+        [Test]
+        public void CarpetOfFlying6x9Percentile()
+        {
+            AssertContent("6 ft. by 9 ft. carpet of flying", 35);
+        }
+
+        [Test]
+        public void AmuletOfMightyFistsPlus3Percentile()
+        {
+            AssertContent("Amulet of mighty fists +3", 36);
+        }
+
+        [Test]
+        public void WingsOfFlyingPercentile()
+        {
+            AssertContent("Wings of flying", 37);
+        }
+
+        [Test]
+        public void CloakOfEtherealnessPercentile()
+        {
+            AssertContent("Cloak of etherealness", 38);
+        }
+
+        [Test]
+        public void DaernsInstantFortressPercentile()
+        {
+            AssertContent("Daern's instant fortress", 39);
+        }
+
+        [Test]
+        public void ManualOfBodilyHealthPlus2Percentile()
+        {
+            AssertContent("Manual of bodily health +2", 40);
+        }
+
+        [Test]
+        public void ManualOfGainfulExercisePlus2Percentile()
+        {
+            AssertContent("Manual of gainful exercise +2", 41);
+        }
+
+        [Test]
+        public void ManualOfQuicknessInActionPlus2Percentile()
+        {
+            AssertContent("Manual of quickness in action +2", 42);
+        }
+
+        [Test]
+        public void TomeOfClearThoughtPlus2Percentile()
+        {
+            AssertContent("Tome of clear thought +2", 43);
+        }
+
+        [Test]
+        public void TomeOfLeadershipAndInfluencePlus2Percentile()
+        {
+            AssertContent("Tome of leadership and influence +2", 44);
+        }
+
+        [Test]
+        public void TomeOfUnderstandingPlus2Percentile()
+        {
+            AssertContent("Tome of understanding +2", 45);
+        }
+
+        [Test]
+        public void EyesOfCharmingPercentile()
+        {
+            AssertContent("Eyes of charming", 46);
+        }
+
+        [Test]
+        public void RobeOfStarsPercentile()
+        {
+            AssertContent("Robe of stars", 47);
+        }
+
+        [Test]
+        public void CarpetOfFlying10x10Percentile()
+        {
+            AssertContent("10 ft. by 10 ft. carpet of flying", 48);
+        }
+
+        [Test]
+        public void DarkskullPercentile()
+        {
+            AssertContent("Darkskull", 49);
+        }
+
+        [Test]
+        public void CubeOfForcePercentile()
+        {
+            AssertContent("Cube of force", 50);
+        }
+
+        [Test]
+        public void BracersOfArmorPlus8Percentile()
+        {
+            AssertContent("Bracers of armor +8", 51);
+        }
+
+        [Test]
+        public void EighthLevelSpellPearlOfPowerPercentile()
+        {
+            AssertContent("8th-level spell pearl of power", 52);
+        }
+
+        [Test]
+        public void CrystalBallWithTelepathyPercentile()
+        {
+            AssertContent("Crystal ball with telepathy", 53);
+        }
+
+        [Test]
+        public void GreaterHornOfBlastingPercentile()
+        {
+            AssertContent("Greater horn of blasting", 54);
+        }
+
+        [Test]
+        public void TwoSpellsPearlOfPowerPercentile()
+        {
+            AssertContent("Two spells pearl of power", 55);
+        }
+
+        [Test]
+        public void HelmOfTeleportationPercentile()
+        {
+            AssertContent("Helm of teleportation", 56);
+        }
+
+        [Test]
+        public void GemOfSeeingPercentile()
+        {
+            AssertContent("Gem of seeing", 57);
+        }
+
+        [Test]
+        public void RobeOfTheArchmagiPercentile()
+        {
+            AssertContent("Robe of the archmagi", 58);
+        }
+
+        [Test]
+        public void MantleOfFaithPercentile()
+        {
+            AssertContent("Mantle of faith", 59);
+        }
+
+        [Test]
+        public void CrystalBallWithTrueSeeingPercentile()
+        {
+            AssertContent("Crystal ball with true seeing", 60);
+        }
+
+        [Test]
+        public void NinthLevelSpellPearlOfPowerPercentile()
+        {
+            AssertContent("9th-level spell pearl of power", 61);
+        }
+
+        [Test]
+        public void WellOfManyWorldsPercentile()
+        {
+            AssertContent("Well of many worlds", 62);
+        }
+
+        [Test]
+        public void ManualOfBodilyHealthPlus3Percentile()
+        {
+            AssertContent("Manual of bodily health +3", 63);
+        }
+
+        [Test]
+        public void ManualOfGainfulExercisePlus3Percentile()
+        {
+            AssertContent("Manual of gainful exercise +3", 64);
+        }
+
+        [Test]
+        public void ManualOfQuicknessInActionPlus3Percentile()
+        {
+            AssertContent("Manual of quickness in action +3", 65);
+        }
+
+        [Test]
+        public void TomeOfClearThoughtPlus3Percentile()
+        {
+            AssertContent("Tome of clear thought +3", 66);
+        }
+
+        [Test]
+        public void TomeOfLeadershipAndInfluencePlus3Percentile()
+        {
+            AssertContent("Tome of leadership and influence +3", 67);
+        }
+
+        [Test]
+        public void TomeOfUnderstandingPlus3Percentile()
+        {
+            AssertContent("Tome of understanding +3", 68);
+        }
+
+        [Test]
+        public void ApparatusOfKwalishPercentile()
+        {
+            AssertContent("Apparatus of Kwalish", 69);
+        }
+
+        [Test]
+        public void MantleOfSpellResistancePercentile()
+        {
+            AssertContent("Mentle of spell resistance", 70);
+        }
+
+        [Test]
+        public void MirrorOfOppositionPercentile()
+        {
+            AssertContent("Mirror of opposition", 71);
+        }
+
+        [Test]
+        public void GreaterStrandOfPrayerBeadsPercentile()
+        {
+            AssertContent("Greater strand of prayer beads", 72);
+        }
+
+        [Test]
+        public void AmuletOfMightyFistsPlus4Percentile()
+        {
+            AssertContent("Amulet of mighty fists +4", 73);
+        }
+
+        [Test]
+        public void EyesOfPetrificationPercentile()
+        {
+            AssertContent("Eyes of petrification", 74);
+        }
+
+        [Test]
+        public void BowlOfCommandingWaterElementalsPercentile()
+        {
+            AssertContent("Bowl of commanding water elementals", 75);
+        }
+
+        [Test]
+        public void BrazierOfCommandingFireElementalsPercentile()
+        {
+            AssertContent("Brazier of commanding fire elementals", 76);
+        }
+
+        [Test]
+        public void CenserOfControllingAirElementalsPercentile()
+        {
+            AssertContent("Censer of controlling air elementals", 77);
+        }
+
+        [Test]
+        public void StoneOfControllingEarthElementalsPercentile()
+        {
+            AssertContent("Stone of controlling earth elementals", 78);
+        }
+
+        [Test]
+        public void ManualOfBodilyHealthPlus4Percentile()
+        {
+            AssertContent("Manual of bodily health +4", 79);
+        }
+
+        [Test]
+        public void ManualOfGainfulExercisePlus4Percentile()
+        {
+            AssertContent("Manual of gainful exercise +4", 80);
+        }
+
+        [Test]
+        public void ManualOfQuicknessInActionPlus4Percentile()
+        {
+            AssertContent("Manual of quickness in action +4", 81);
+        }
+
+        [Test]
+        public void TomeOfClearThoughtPlus4Percentile()
+        {
+            AssertContent("Tome of clear thought +4", 82);
+        }
+
+        [Test]
+        public void TomeOfLeadershipAndInfluencePlus4Percentile()
+        {
+            AssertContent("Tome of leadership and influence +4", 83);
+        }
+
+        [Test]
+        public void TomeOfUnderstandingPlus4Percentile()
+        {
+            AssertContent("Tome of understanding +4", 84);
+        }
+
+        [Test]
+        public void AmuletOfThePlanesPercentile()
+        {
+            AssertContent("Amulet of the planes", 85);
+        }
+
+        [Test]
+        public void RobeOfEyesPercentile()
+        {
+            AssertContent("Robe of eyes", 86);
+        }
+
+        [Test]
+        public void HelmOfBrilliancePercentile()
+        {
+            AssertContent("Helm of brilliance", 87);
+        }
+
+        [Test]
+        public void ManualOfBodilyHealthPlus5Percentile()
+        {
+            AssertContent("Manual of bodily health +5", 88);
+        }
+
+        [Test]
+        public void ManualOfGainfulExercisePlus5Percentile()
+        {
+            AssertContent("Manual of gainful exercise +5", 89);
+        }
+
+        [Test]
+        public void ManualOfQuicknessInActionPlus5Percentile()
+        {
+            AssertContent("Manual of quickness in action +5", 90);
+        }
+
+        [Test]
+        public void TomeOfClearThoughtPlus5Percentile()
+        {
+            AssertContent("Tome of clear thought +5", 91);
+        }
+
+        [Test]
+        public void TomeOfLeadershipAndInfluencePlus5Percentile()
+        {
+            AssertContent("Tome of leadership and influence +5", 92);
+        }
+
+        [Test]
+        public void TomeOfUnderstandingPlus5Percentile()
+        {
+            AssertContent("Tome of understanding +5", 93);
+        }
+
+        [Test]
+        public void EfreetiBottlePercentile()
+        {
+            AssertContent("Efreeti bottle", 94);
+        }
+
+        [Test]
+        public void AmuletOfMightyFistsPlus5Percentile()
+        {
+            AssertContent("Amulet of mighty fists +5", 95);
+        }
+
+        [Test]
+        public void ChaosDiamondPercentile()
+        {
+            AssertContent("Chaos diamond", 96);
+        }
+
+        [Test]
+        public void CubicGatePercentile()
+        {
+            AssertContent("Cubic gate", 97);
+        }
+
+        [Test]
+        public void IronFlaskPercentile()
+        {
+            AssertContent("Iron flask", 98);
+        }
+
+        [Test]
+        public void MirrorOfMentalProwessPercentile()
+        {
+            AssertContent("Mirror of mental prowess", 99);
+        }
+
+        [Test]
+        public void MirrorOfLifeTrappingPercentile()
+        {
+            AssertContent("Mirror of life trapping", 100);
+        }
     }
 }
