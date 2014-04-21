43bba92673d42275b47bd81bd0af89a1c861c5b8
Karl Speer <cidthecoatrack@gmail.com>
2014-04-20 23:21:31 -0400

    More specific gear tables

---
+++ Tests/Integration/Tables/Items/Magical/Armor/Medium/MediumSpecificArmorsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using EquipmentGen.Common.Items;
+using NUnit.Framework;
+
+namespace EquipmentGen.Tests.Integration.Tables.Items.Magical.Armor.Medium
+{
+    [TestFixture]
+    public class MediumSpecificArmorsTests : PercentileTests
+    {
+        protected override String tableName
+        {
+            get { return "MediumSpecificArmors"; }
+        }
+
+        [TestCase(ArmorConstants.ChainShirt, 0, 1, 25)]
+        [TestCase(ArmorConstants.FullPlate, 0, 26, 45)]
+        [TestCase(ArmorConstants.ElvenChain, 0, 46, 57)]
+        [TestCase(ArmorConstants.RhinoHide, 2, 58, 67)]
+        [TestCase(ArmorConstants.Breastplate, 0, 68, 82)]
+        [TestCase(ArmorConstants.DwarvenPlate, 0, 83, 97)]
+        [TestCase(ArmorConstants.BandedMailOfLuck, 3, 98, 100)]
+        public void Percentile(String armor, Int32 bonus, Int32 lower, Int32 upper)
+        {
+            var content = String.Format("{0},{1}", armor, bonus);
+            AssertPercentile(content, lower, upper);
+        }
+    }
+}
---
+++ Tests/Integration/Tables/Items/Magical/Armor/SpecificShieldsSpecialAbilitiesTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using EquipmentGen.Common.Items;
+using NUnit.Framework;
+
+namespace EquipmentGen.Tests.Integration.Tables.Items.Magical.Armor
+{
+    [TestFixture]
+    public class SpecificShieldsSpecialAbilitiesTests : AttributesTests
+    {
+        protected override String tableName
+        {
+            get { return "SpecificShieldsSpecialAbilities"; }
+        }
+
+        [TestCase(ArmorConstants.AbsorbingShield, ItemTypeConstants.Armor,
+                                                  AttributeConstants.Shield,
+                                                  AttributeConstants.NotTower,
+                                                  AttributeConstants.Specific,
+                                                  AttributeConstants.Metal)]
+        [TestCase(ArmorConstants.Buckler, ItemTypeConstants.Armor,
+                                          AttributeConstants.Shield,
+                                          AttributeConstants.NotTower,
+                                          AttributeConstants.Specific,
+                                          AttributeConstants.Wood)]
+        [TestCase(ArmorConstants.HeavyWoodenShield, ItemTypeConstants.Armor,
+                                                    AttributeConstants.Shield,
+                                                    AttributeConstants.NotTower,
+                                                    AttributeConstants.Specific,
+                                                    AttributeConstants.Wood)]
+        [TestCase(ArmorConstants.HeavySteelShield, ItemTypeConstants.Armor,
+                                                   AttributeConstants.Shield,
+                                                   AttributeConstants.NotTower,
+                                                   AttributeConstants.Specific,
+                                                   AttributeConstants.Metal)]
+        [TestCase(ArmorConstants.CastersShield, ItemTypeConstants.Armor,
+                                                AttributeConstants.Shield,
+                                                AttributeConstants.NotTower,
+                                                AttributeConstants.Specific,
+                                                AttributeConstants.Wood)]
+        [TestCase(ArmorConstants.SpinedShield, ItemTypeConstants.Armor,
+                                               AttributeConstants.Shield,
+                                               AttributeConstants.NotTower,
+                                               AttributeConstants.Specific,
+                                               AttributeConstants.Metal)]
+        [TestCase(ArmorConstants.LionsShield, ItemTypeConstants.Armor,
+                                              AttributeConstants.Shield,
+                                              AttributeConstants.NotTower,
+                                              AttributeConstants.Specific,
+                                              AttributeConstants.Metal)]
+        [TestCase(ArmorConstants.WingedShield, ItemTypeConstants.Armor,
+                                               AttributeConstants.Shield,
+                                               AttributeConstants.NotTower,
+                                               AttributeConstants.Specific,
+                                               AttributeConstants.Wood)]
+        public void Attributes(String name, params String[] attributes)
+        {
+            AssertAttributes(name, attributes);
+        }
+    }
+}
