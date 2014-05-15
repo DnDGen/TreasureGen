f332afaba4812de3786bc7044c51d050b7861de1
Karl Speer <kspeer@qualtrax.com>
2014-05-15 17:04:33 -0400

    Begin weapon generator
    * Finish major potions
    * Begin magical weapon generator

---
+++ Tests/Integration/Stress/Items/Magical/MagicalWeaponGeneratorTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using EquipmentGen.Common.Items;
+using EquipmentGen.Generators.Interfaces.Items.Magical;
+using Ninject;
+using NUnit.Framework;
+
+namespace EquipmentGen.Tests.Integration.Stress.Items.Magical
+{
+    [TestFixture]
+    public class MagicalWeaponGeneratorTests : StressTests
+    {
+        [Inject, Named(ItemTypeConstants.Weapon)]
+        public IMagicalItemGenerator MagicalWeaponGenerator { get; set; }
+
+        [Test]
+        public void StressedMagicalWeaponGenerator()
+        {
+            StressGenerator();
+        }
+
+        protected override void MakeAssertions()
+        {
+            var power = GetNewPower(false);
+            var weapon = MagicalWeaponGenerator.GenerateAtPower(power);
+
+            if (weapon.ItemType == ItemTypeConstants.SpecificCursedItem)
+                return;
+
+            Assert.That(weapon.Name, Is.Not.Empty);
+            Assert.That(weapon.Attributes, Contains.Item(AttributeConstants.Bludgeoning).Or.Contains(AttributeConstants.NotBludgeoning));
+            Assert.That(weapon.Attributes, Contains.Item(AttributeConstants.Common).Or.Contains(AttributeConstants.Uncommon));
+            Assert.That(weapon.Attributes, Contains.Item(AttributeConstants.Melee).Or.Contains(AttributeConstants.Ranged));
+            Assert.That(weapon.Contents, Is.Empty);
+            Assert.That(weapon.ItemType, Is.EqualTo(ItemTypeConstants.Weapon));
+
+            if (weapon.IsMagical && !weapon.Attributes.Contains(AttributeConstants.Specific))
+                Assert.That(weapon.Magic.Bonus, Is.GreaterThan(0), weapon.Name);
+
+            if (weapon.Attributes.Contains(AttributeConstants.Ammunition))
+                Assert.That(weapon.Quantity, Is.InRange<Int32>(1, 50));
+            else
+                Assert.That(weapon.Quantity, Is.EqualTo(1));
+
+            Assert.That(weapon.Traits, Is.Not.Null);
+        }
+    }
+}
---
+++ Tests/Unit/Generators/Items/Magical/MagicalWeaponGeneratorTests.cs
@@ -0,0 +1,27 @@
+﻿using EquipmentGen.Common.Items;
+using EquipmentGen.Generators.Interfaces.Items.Magical;
+using EquipmentGen.Generators.Items.Magical;
+using NUnit.Framework;
+
+namespace EquipmentGen.Tests.Unit.Generators.Items.Magical
+{
+    [TestFixture]
+    public class MagicalWeaponGeneratorTests
+    {
+        private IMagicalItemGenerator weaponGenerator;
+
+        [SetUp]
+        public void Setup()
+        {
+            weaponGenerator = new MagicalWeaponGenerator();
+        }
+
+        [Test]
+        public void GenerateWeapon()
+        {
+            var weapon = weaponGenerator.GenerateAtPower("power");
+            Assert.That(weapon.ItemType, Is.EqualTo(ItemTypeConstants.Weapon));
+            Assert.That(weapon.Quantity, Is.AtLeast(1));
+        }
+    }
+}
