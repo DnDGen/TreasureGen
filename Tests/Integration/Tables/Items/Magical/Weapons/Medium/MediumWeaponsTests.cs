2ad21c7ccdb3160e860439711326d75b44195fc1
Karl Speer <cidthecoatrack@gmail.com>
2014-05-16 21:50:35 -0400

    Medium weapons

---
+++ Tests/Integration/Tables/Items/Magical/Weapons/Medium/MediumWeaponsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace EquipmentGen.Tests.Integration.Tables.Items.Magical.Weapons.Medium
+{
+    [TestFixture]
+    public class MediumWeaponsTests : PercentileTests
+    {
+        protected override String tableName
+        {
+            get { return "MediumWeapons"; }
+        }
+
+        [TestCase("SpecificWeapon", 63, 68)]
+        [TestCase("SpecialAbility", 69, 100)]
+        public void Percentile(String content, Int32 lower, Int32 upper)
+        {
+            AssertPercentile(content, lower, upper);
+        }
+
+        [TestCase(1, 1, 10)]
+        [TestCase(2, 11, 29)]
+        [TestCase(3, 30, 58)]
+        [TestCase(4, 59, 62)]
+        public void Percentile(Int32 bonus, Int32 lower, Int32 upper)
+        {
+            var content = Convert.ToString(bonus);
+            AssertPercentile(content, lower, upper);
+        }
+    }
+}
