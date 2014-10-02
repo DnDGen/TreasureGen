ac104e8732ab3b2dbece348fb950ae3f3b0312a8
Karl Speer <kspeer@qualtrax.com>
2014-10-02 12:32:23 -0400

    Curse table name refactor
    * Specific item is no longer an item type, so that traits table can die

--- Tests/Integration/Tables/Items/Magical/Curses/CursedDependentSituationsTests.cs
+++ Tests/Integration/Tables/Items/Magical/Curses/CursedDependentSituationsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using EquipmentGen.Tables.Interfaces;
 using NUnit.Framework;
 
 namespace EquipmentGen.Tests.Integration.Tables.Items.Magical.Curses
@@ -8,7 +9,7 @@ namespace EquipmentGen.Tests.Integration.Tables.Items.Magical.Curses
     {
         protected override String tableName
         {
-            get { return "CursedDependentSituations"; }
+            get { return TableNameConstants.Percentiles.Set.CursedDependentSituations; }
         }
 
         [Test]
