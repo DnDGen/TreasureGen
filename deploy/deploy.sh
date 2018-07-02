4fe90876d9621e7d4c4d8d5a9660aab04dfa06d2
Karl Speer <cidthecoatrack@gmail.com>
2018-07-02 12:57:04 -0400

    Feature #66 - Travis Build Update
    * Use latest mono
    * Download newest nuget.exe and use that
    * Use msbuild instead of xbuild

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,7 +8,7 @@ echo "Nuget Source is $Source"
 echo "Nuget API Key is $ApiKey (should be secure)"
 
 echo "Packing TreasureGen"
-nuget pack ./TreasureGen/TreasureGen.nuspec -Verbosity detailed
+mono ./nuget.exe pack ./TreasureGen/TreasureGen.nuspec -Verbosity detailed
 
 echo "Pushing TreasureGen"
-nuget push ./DnDGen.TreasureGen.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
+mono ./nuget.exe push ./DnDGen.TreasureGen.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
